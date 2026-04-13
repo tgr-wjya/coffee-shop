8965b76e62cd857754dfc8ff136d7ca29c47f1a9
Tegar Wijaya Kusuma <64220909+tgr-wjya@users.noreply.github.com>
2026-04-13 21:56:28 +0700

    Add environment configuration, HTTP request examples, and initial service structure

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,14 @@
  */
 
 import { Hono } from "hono";
+import { coffeeShopRouter } from "./routes/coffee";
 
 const app = new Hono();
 
 app.get("/hello", (c) => {
 	return c.text("Hello, World");
 });
+
+app.route("/products", coffeeShopRouter());
 
 export default app;
--- src/routes/coffee.ts
+++ src/routes/coffee.ts
@@ -3,3 +3,15 @@
  * @date 13 April 2026
  */
 
+import { Hono } from "hono";
+import { ProductService } from "../service/product.service";
+
+export function coffeeShopRouter(service = new ProductService()) {
+	const coffeeShop = new Hono();
+
+	coffeeShop.get("/", async (c) => {
+		return c.json(await service.listAllProducts(), 200);
+	});
+
+	return coffeeShop;
+}
